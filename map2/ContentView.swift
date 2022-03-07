e4737a951a0b63e47e5e5f2729e07988585b3154
action-ukon <actionukon0614mhf@gmail.com>
2022-03-07 23:53:30 +0900

    aptest

--- map2/ContentView.swift
+++ map2/ContentView.swift
@@ -6,13 +6,23 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    // managerの更新を観測する
+    @ObservedObject var manager = LocationManager()
+    // ユーザートラッキングモード（追従モード）
+    @State var trackingMode = MapUserTrackingMode.follow
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // 現在地を追従する地図を表示する
+        Map(coordinateRegion: $manager.region,
+            showsUserLocation: true,
+            userTrackingMode: $trackingMode)
+            .edgesIgnoringSafeArea(.bottom)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
